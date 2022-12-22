e41710711ebd1f1995749e27953b6974b2fa1aeb
Igor-S-M <igorsousa3148@gmail.com>
2022-12-22 17:19:33 -0300

    feat: get ranking

--- src/routes/rankingRoute/rankingRoute.js
+++ src/routes/rankingRoute/rankingRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getRank } from "../../controllers/rankingControllers/ranking.controllers.js";
 
 const rankingRouter = express.Router();
+
+rankingRouter.get('/ranking',getRank)
 
 export default rankingRouter;
